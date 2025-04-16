d7e8d486b92b4c09f25f907dd60bfd3a80bf7674
FacuMorales <facu44515119@gmail.com>
2025-04-16 19:48:41 -0300

    landing hecha y login/register casi terminado

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,35 +1,201 @@
-'use client'
-import { useEffect } from 'react'
+'use client';
+
+import { useState } from 'react';
 import { useRouter } from 'next/navigation'
 import { supabase } from '@/services/supabaseClient'
+import { Eye, EyeOff, Lock, User } from 'lucide-react';
 import "@/app/globals.css"
 
-export default function LoginPage() {
-  const router = useRouter()
+export default function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    remember: false,
+  });
 
-  useEffect(() => {
-    const checkUser = async () => {
-      const { data: { session } } = await supabase.auth.getSession()
-      if (session) {
-        const { data: perfil } = await supabase
-          .from('users')
-          .select('role')
-          .eq('id', session.user.id)
-          .single()
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+  });
 
-        if (perfil?.role === 'cliente') router.push('/dashboard/cliente')
-        else if (perfil?.role === 'pm') router.push('/dashboard/pm')
-        else if (perfil?.role === 'diseñador') router.push('/dashboard/diseñador')
+  const [loginError, setLoginError] = useState(''); // Para mostrar el error de login
+
+  const [isLogin, setIsLogin] = useState(true); // true = login, false = register
+
+  const [showPassword, setShowPassword] = useState(false); // Estado para mostrar/ocultar la contraseña
+
+  const router = useRouter();
+
+  const validateForm = () => {
+    const newErrors = {
+      email: formData.email ? '' : 'El nombre de usuario es obligatorio.',
+      password: formData.password ? '' : 'La contraseña es obligatoria.',
+    };
+    setErrors(newErrors);
+    return !newErrors.email && !newErrors.password;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+  
+    setLoginError(''); // Limpiar errores previos
+  
+    const { email, password } = formData;
+  
+    if (isLogin) {
+      // LOGIN
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+  
+      if (error) {
+        setLoginError('Email o contraseña incorrectos.');
+      } else {
+        router.push('/dashboard/cliente');
+      }
+  
+    } else {
+      // REGISTER
+      const { error } = await supabase.auth.signUp({
+        email,
+        password,
+      });
+  
+      if (error) {
+        setLoginError(error.message);
+      } else {
+        router.push('/dashboard/cliente');
       }
     }
+  };
 
-    checkUser();
-    
-  }, [])
+  const handleIrALanding = () => {
+    router.push('/');
+  };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-purple-100">
-      <h1 className="text-4xl text-red-500 font-bold bg-yellow-300">LOGIN</h1>
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="flex w-full max-w-5xl overflow-hidden rounded-3xl bg-white shadow-xl">
+        <div className="hidden w-5/12 bg-gradient-to-br from-yellow-100 via-cyan-200 to-white lg:block "></div>
+        <div className="w-full p-16 px-12 lg:px-[75px] lg:w-7/12">
+          <div className="flex justify-center">
+            <img
+              onClick={handleIrALanding}
+              src="https://grayola.io/wp-content/uploads/2024/05/Grayola-Logo-SVG.svg"
+              alt="grayola"
+              width="50%"
+              className='cursor-pointer'
+            />
+          </div>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-5 w-full mt-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="text-sm text-gray-700 font-bold"
+              >
+                Email
+              </label>
+              <div className="relative mt-2">
+                <User className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-400" />
+                <input
+                  id='email'
+                  type="text"
+                  placeholder='Ingrese su email'
+                  className={
+                    'flex h-10 w-full bg-background px-3 py-2 focus-visible:outline-none pl-10 border-0 rounded-none border-b-2 border-[#BFBFBF]'
+                  }
+                  onChange={(e) => {
+                    setFormData({ ...formData, email: e.target.value });
+                    // Validar en tiempo real
+                    setErrors((prevErrors) => ({
+                      ...prevErrors,
+                      email: e.target.value
+                        ? ''
+                        : 'El email es obligatorio.',
+                    }));
+                  }}
+                />
+                {errors.email && (
+                  <p className="text-xs text-red-500">{errors.email}</p>
+                )}
+              </div>
+            </div>
+
+            <div className="mt-2">
+              <label
+                htmlFor="password"
+                className="text-sm text-gray-700 font-bold"
+              >
+                Contraseña
+              </label>
+              <div className="relative mt-2">
+                <Lock className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-400" />
+                <input
+                  id='password'
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder='Ingrese su contraseña'
+                  className={
+                    'flex h-10 w-full bg-background px-3 py-2 focus-visible:outline-none pl-10 border-0 rounded-none border-b-2 border-[#BFBFBF]'
+                  }
+                  onChange={(e) => {
+                    setFormData({ ...formData, password: e.target.value });
+                    setErrors((prevErrors) => ({
+                      ...prevErrors,
+                      password: e.target.value
+                        ? ''
+                        : 'La contraseña es obligatoria.',
+                    }));
+                  }}
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)} // Alternar el estado
+                  className="absolute right-3 top-1/2 -translate-y-1/2"
+                >
+                  {showPassword ? (
+                    <EyeOff className="h-4 w-4 text-gray-400" />
+                  ) : (
+                    <Eye className="h-4 w-4 text-gray-400" />
+                  )}
+                </button>
+                {errors.password && (
+                  <p className="text-xs text-red-500">{errors.password}</p>
+                )}
+              </div>
+            </div>
+
+            {/* Mostrar el error del login */}
+            {loginError && (
+              <p className="text-xs text-red-500 mt-2">{loginError}</p>
+            )}
+
+            <button
+              type="submit"
+              className={`font-semibold px-6 py-3 rounded-full border border-black transition mt-6
+                ${!formData.email || !formData.password
+                  ? 'bg-[#9AFF1A] opacity-50 text-gray-500 cursor-not-allowed'
+                  : 'bg-[#9AFF1A] text-black shadow-black shadow-lg hover:bg-[#D06CFF] cursor-pointer'}
+              `}
+              disabled={ !formData.email || !formData.password }
+            >
+              {isLogin ? "Iniciar Sesión" : "Registrarse"}
+            </button>
+            
+            <p className="mt-4 text-sm text-center text-gray-600">
+              {isLogin ? '¿No tenés cuenta?' : '¿Ya tenés cuenta?'}{' '}
+              <span
+                onClick={() => setIsLogin(!isLogin)}
+                className="text-blue-600 hover:underline cursor-pointer"
+              >
+                {isLogin ? 'Registrate' : 'Iniciar sesión'}
+              </span>
+            </p>
+
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,35 @@
+'use client'
+import { useEffect } from 'react'
+import { supabase } from '@/services/supabaseClient'
 import './globals.css'
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-purple-100">
-      <h1 className="text-4xl text-red-500 font-bold bg-yellow-300">Hola Grayola</h1>
-      <p className="text-4xl font-bold text-red-200">Hola</p>
-    </main>
+    <div className="min-h-screen bg-gradient-to-br from-yellow-100 via-cyan-200 to-white text-black">
+      <header className="flex justify-between items-center p-6">
+        <img
+          src={
+            'https://grayola.io/wp-content/uploads/2024/05/Grayola-Logo-SVG.svg'
+          }
+          alt={"grayola"}
+          className="h-12"
+        />
+      </header>
+
+      <main className="flex flex-col items-center text-center px-6 mt-20 font-[swear-display-cilati] max-w-5xl mx-auto">
+        <h2 className="text-6xl md:text-7xl font-black leading-tight mb-6 tracking-wide">
+          Aplicación web para <br /> gestión de proyectos <br /> de diseño
+        </h2>
+        <p className="max-w-xl text-xl mb-8 text-gray-700">
+          Escalá y delegá todas las operaciones de diseño de forma rápida y sencilla, sin preocuparte por contratar o administrar recursos adicionales.
+        </p>
+        <a
+          href="/login"
+          className="bg-[#9AFF1A] shadow-black shadow-lg text-black font-semibold px-6 py-3 rounded-full border border-black hover:bg-[#D06CFF] transition"
+        >
+          Iniciar sesión
+        </a>
+      </main>
+    </div>
   );
 }
